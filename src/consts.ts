65d1ec6d8b39c677805bc907f779955a702ab8f0
wenyen.huang <wenyen.huang@ailabs.tw>
2024-08-21 18:10:52 +0800

    chore: website setting

--- src/consts.ts
+++ src/consts.ts
@@ -3,9 +3,8 @@ import type { Site, Page, Links, Socials } from "@types";
 // Global
 export const SITE: Site = {
   TITLE: "Astro Sphere",
-  DESCRIPTION:
-    "Welcome to Astro Sphere, a portfolio and blog for designers and developers.",
-  AUTHOR: "Mark Horn",
+  DESCRIPTION: "Welcome to Astro Sphere, this is my portfolio practice.",
+  AUTHOR: "Wen Yen Huang",
 };
 
 // Work Page
